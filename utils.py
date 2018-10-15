dd5b5977a1b4c976ac49c8b526750e0a2b855dd0
Sami Ede <sami.ede@icloud.com>
2018-10-15 11:09:02 +0200

    Cuda in utils

--- utils.py
+++ utils.py
@@ -66,7 +66,7 @@ class Logger:
         if torch.cuda.is_available():
             images.cuda()
             relevance.cuda()
-        images = torch.cat((images, relevance))
+        images = torch.cat(images, relevance)
 
         # Make horizontal grid from image tensor
         horizontal_grid = vutils.make_grid(
