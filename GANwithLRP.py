3215c70db5c55762431076ceaa423bca3b2bf75a
Sami Ede <sami.ede@icloud.com>
2018-10-25 18:55:24 +0200

    Normalize back to 0.5

--- GANwithLRP.py
+++ GANwithLRP.py
@@ -17,15 +17,6 @@ def load_mnist_data():
     out_dir = './dataset'
     return datasets.MNIST(root=out_dir, train=True, transform=transform, download=True)
 
-
-def images_to_vectors(images):
-    return images.view(images.size(0), 784)
-
-
-def vectors_to_images(vectors):
-    return vectors.view(vectors.size(0), 1, 28, 28)
-
-
 def noise(size):
     """
 
