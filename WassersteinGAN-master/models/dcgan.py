71cf1142509efdf22f93b49ede64f474085c0ccf
Sami Ede <sami.ede@icloud.com>
2018-12-04 09:49:17 +0100

    typo

--- WassersteinGAN-master/models/dcgan.py
+++ WassersteinGAN-master/models/dcgan.py
@@ -62,30 +62,30 @@ class DCGAN_D1(nn.Module):
         self.ngpu = ngpu
         assert isize % 16 == 0, "isize has to be a multiple of 16"
 
-        net = RelevanceNet()
-        net.add_module('initial-conv{0}-{1}'.format(nc, ndf),
+        main = RelevanceNet()
+        main.add_module('initial-conv{0}-{1}'.format(nc, ndf),
                        FirstConvolution(nc, ndf, 4, 2, 1))
-        net.add_module('initial-relu{0}'.format(ndf),
+        main.add_module('initial-relu{0}'.format(ndf),
                        PropReLu(inplace=True))
         csize, cndf = isize / 2, ndf
 
         # Extra layers
         for t in range(n_extra_layers):
-            net.add_module('extra-layers-{0}-{1}-conv'.format(t, cndf),
+            main.add_module('extra-layers-{0}-{1}-conv'.format(t, cndf),
                            NextConvolution(cndf, cndf, 3, 1, 1))
-            net.add_module('extra-layers-{0}-{1}-batchnorm'.format(t, cndf),
+            main.add_module('extra-layers-{0}-{1}-batchnorm'.format(t, cndf),
                            BatchNorm2d(cndf))
-            net.add_module('extra-layers-{0}-{1}-relu'.format(t, cndf),
+            main.add_module('extra-layers-{0}-{1}-relu'.format(t, cndf),
                            PropReLu(inplace=True))
 
         while csize > 4:
             in_feat = cndf
             out_feat = cndf * 2
-            net.add_module('pyramid-{0}-{1}-conv'.format(in_feat, out_feat),
+            main.add_module('pyramid-{0}-{1}-conv'.format(in_feat, out_feat),
                            NextConvolution(in_feat, out_feat, 4, 2, 1))
-            net.add_module('pyramid-{0}-batchnorm'.format(out_feat),
+            main.add_module('pyramid-{0}-batchnorm'.format(out_feat),
                            BatchNorm2d(out_feat))
-            net.add_module('pyramid-{0}-relu'.format(out_feat),
+            main.add_module('pyramid-{0}-relu'.format(out_feat),
                            PropReLu(inplace=True))
             cndf = cndf * 2
             csize = csize / 2
@@ -93,9 +93,9 @@ class DCGAN_D1(nn.Module):
         # We take relevance here
         # state size. K x 4 x 4
         # Global average to single output
-        net.add_module('final-{0}-{1}-conv'.format(cndf, 1),
+        main.add_module('final-{0}-{1}-conv'.format(cndf, 1),
                        NextConvolution(cndf, 1, 4, 1, 0))
-        self.net = net
+        self.main = main
 
     def forward(self, input):
         if isinstance(input.data, torch.cuda.FloatTensor) and self.ngpu > 1:
