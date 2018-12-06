9f0cf5d047280a6ca42fb1a799aceb52afeb00de
Sami Ede <sami.ede@icloud.com>
2018-12-06 08:59:01 +0100

    Cudafy

--- WassersteinGAN-master/ModuleRedefinitions.py
+++ WassersteinGAN-master/ModuleRedefinitions.py
@@ -9,6 +9,7 @@ class FirstConvolution(nn.Conv2d):
     def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=2, dilation=1, groups=1,
                  bias=True):
         super().__init__(in_channels, out_channels, kernel_size, stride, padding, dilation, groups, bias)
+        self.cuda()
 
         # Variables for Relevance Propagation
         self.X = None
